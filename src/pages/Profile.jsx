22dcc905c82b81970a978bb3f4aee5088b825d28
Sah Abhishek Kumar <akroy2456@gmail.com>
2025-01-23 10:13:07 +0530

    spelling changed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "../context/NewAuthContext";
 import Orders from "./Orders";
 import { Title } from "../components/Title";
 import { Pencil, PencilOff, Loader2 } from "lucide-react";
-import ProfileInputField from "../components/ProfileInputField";
+import ProfileInputField from "../components/profileInputField";
 
 const Profile = () => {
   const { user, isLoading, updateUser, logout } = useAuth();
