79ce8043384c279c5a9c48ff59a6ff57e3125ee3
Sah Abhishek Kumar <akroy2456@gmail.com>
2025-01-25 11:56:32 +0530

    update server url

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "http://localhost:9000"; //    https://store-backend-zz4k.onrender.com
+export const BASE_URL = "https://store-backend-zz4k.onrender.com"; //      http://localhost:9000
