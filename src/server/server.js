76493e0ae7db8afcc31bc3e0df1d30f7689361ec
Sah Abhishek Kumar <akroy2456@gmail.com>
2025-06-26 17:38:13 +0530

    updated env

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:9000"; //                https://store-backend-zz4k.onrender.com
+// export const BASE_URL = "http://localhost:9000"; //                https://store-backend-zz4k.onrender.com
+export const BASE_URL = "https://store-backend-zz4k.onrender.com";
